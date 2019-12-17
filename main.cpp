52a8a99366e5af5a521ce9630884ae0522e875ad
GaryBlackbourne <gergely.koloszar@gmail.com>
2019-12-17 22:38:42 +0100

    basic argument list implemented,

--- main.cpp
+++ main.cpp
@@ -10,30 +10,73 @@
 
 int main(int argc, char* argv[])
 {
+	std::string inputFile = "defaultIn.wav";
+	std::string outputFile = "defaultOut.wav";
+	std::vector<double> numParam;
+	std::vector<double> denParam;
+	//System sys;
+
+	for (int i = 1; i < argc; i ++){
+		if(std::string{argv[i]} == "--input") {
+			if(argc > i + 1){
+				i++;
+				inputFile = argv[i];
+			}
+		}else if(std::string{argv[i]} == "--output"){
+			if(argc > i + 1){
+				i++;
+				outputFile = argv[i];
+			}
+		}else if(std::string{argv[i]} == "--num"){
+			bool run = true;
+			i++;
+			while(run && argc > i){
+				try
+				{
+					numParam.push_back(std::stod(argv[i]));
+					i++;
+				}catch (std::invalid_argument&){
+					run = false;
+					i--;
+				}
+			}
+		}else if(std::string{argv[i]} == "--den"){
+			bool run = true;
+			i++;
+			while(run && argc > i){
+				try
+				{
+					denParam.push_back(std::stod(argv[i]));
+					i++;
+				}catch (std::invalid_argument&){
+					run = false;
+					i--;
+				}
+			}
+		}else{
+			std::cout << "Invalid argument: " << argv[i] << std::endl;
+		}
+	}
+
+
+
+
+
 
 //	Signal input;
 //	IO io;
 //	io.read("test.wav", input);
 
 
-	System sys{{100},{0.4,0.3,0.2}};
-
-	Signal v = {0,0,1,0,0,0,0,0,0,0,0};
-
-	sys.reset();
-	Signal ans = sys.eval(v);
-
-	for(auto i : ans){
-		std::cout << i << std::endl;
-	}
-
+//	System sys{{100},{0.4,0.3,0.2}};
 //
-//	if(argv[0] == "start"){
-//		std::cout << "start" << std::endl;
-//	}else if(argv[0] == "valami"){
-//		std::cout << "valami" << std::endl;
-//	}else {
-//		std::cout << "unknown command" << std::endl;
+//	Signal v = {0,0,1,0,0,0,0,0,0,0,0};
+//
+//	sys.reset();
+//	Signal ans = sys.eval(v);
+//
+//	for(auto i : ans){
+//	s	std::cout << i << std::endl;
 //	}
 
 
